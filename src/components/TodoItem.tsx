1e23436516d3f33a7d8b98eb1d83f4421bcbd751
bedirgcmz <bdrgcmz@gmail.com>
2024-08-06 21:32:40 +0200

    Fix some styled

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -30,22 +30,34 @@ const TodoItemContainer = styled.li<{ completed: boolean }>`
 
   span {
     text-decoration: ${({ completed }) => (completed ? "line-through" : "none")};
+    min-width: 43px;
+  }
+
+  span.flex {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
   }
 `;
 
 const FaTrashAltStyles = styled.i`
   color: #b43f3f;
+  padding-top: 4px;
 `;
 const FaCheckCircleStyles = styled.i<{ completed: boolean }>`
   color: ${({ completed }) => (completed ? "#f6f6f6" : "#DD5746")};
-  margin-right: 12px;
+  /* margin-right: 12px; */
+  font-size: 19px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 const TodoItem: React.FC<TodoItemProps> = ({ todo, onToggle, onDelete }) => {
   return (
     <TodoItemContainer completed={todo.completed} onClick={() => onToggle(todo.id)}>
       <span>{todo.text}</span>
-      <span>
+      <span className="flex">
         <FaCheckCircleStyles completed={todo.completed}>
           {todo.completed ? <MdFactCheck /> : <MdCheckBoxOutlineBlank />}
         </FaCheckCircleStyles>
